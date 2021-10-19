06694eb5850dc79f52f6ad517bcea8489b01e925
Tiago Nunes <tiago.nunes.lx@gmail.com>
2021-10-19 15:48:32 +0100

    Fix test

--- src/Components/app/app.test.js
+++ src/Components/app/app.test.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { render } from '@testing-library/react'
 import App from './app'
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  render(<App />, div)
 })
