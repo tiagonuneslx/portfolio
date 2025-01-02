4403f7d11883c59b5cde37945b220ef594095f76
tiagonuneslx <tiago.nunes.lx@gmail.com>
2025-01-02 18:16:35 +0000

    Added education

--- src/Components/workexp/workexp.js
+++ src/Components/workexp/workexp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './workexp.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarAlt, faEllipsisH, faGraduationCap } from '@fortawesome/free-solid-svg-icons'
 import { DotPattern } from '../dotpattern/dotpattern'
 
 export default class WorkExp extends Component {
@@ -12,7 +12,7 @@ export default class WorkExp extends Component {
         style={{
           backgroundImage: 'url(' + process.env.PUBLIC_URL + '/dark_blue_gradient.svg)',
           backgroundSize: 'cover',
-          position: 'relative'
+          position: 'relative',
         }}
       >
         <DotPattern alpha="0.5" />
@@ -21,6 +21,23 @@ export default class WorkExp extends Component {
                style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
             <div className="exprow" style={{ display: 'flex' }}>
               <ul className="cbp_tmtimeline">
+                <li>
+                  <div className="cbp_tmicon education">
+                    <FontAwesomeIcon icon={faGraduationCap} color="white" />
+                  </div>
+                  <div className="cbp_tmlabel wow fadeInRight animated education"
+                       style={{
+                         backgroundImage: 'url(' + process.env.PUBLIC_URL + '/dark_gray_gradient.svg)',
+                         paddingTop: '12px',
+                         paddingBottom: '24px',
+                         minHeight: '0',
+                       }}>
+                    <div className="projectParagraph">
+                      <p>In 2024, I got my <strong style={{fontWeight: 'bold'}}>Master&apos;s in Computer Engineering</strong>,
+                        from ISCTE-Lisbon, Portugal, specializing in Intelligent Systems and Data Science.</p>
+                    </div>
+                  </div>
+                </li>
                 <li>
                   <div className="cbp_tmicon">
                     <img src={process.env.PUBLIC_URL + '/bryj_logo.png'} alt="Bryj Logo" width="80"
@@ -49,7 +66,8 @@ export default class WorkExp extends Component {
                 </li>
                 <li>
                   <div className="cbp_tmicon">
-                    <img src={process.env.PUBLIC_URL + '/quatenus_logo.png'}  alt="Quatenus Logo" width="80" height="80" />
+                    <img src={process.env.PUBLIC_URL + '/quatenus_logo.png'} alt="Quatenus Logo" width="80"
+                         height="80" />
                   </div>
                   <div className="cbp_tmlabel wow fadeInRight animated"
                        style={{ backgroundImage: 'url(' + process.env.PUBLIC_URL + '/dark_gray_gradient.svg)' }}>
@@ -59,9 +77,57 @@ export default class WorkExp extends Component {
                       2021
                     </div>
                     <div className="projectParagraph">
-                      <p>Worked for a IoT company in the development of applications for multiple types of Android devices, including Smartphones, Tablets, TVs, and custom devices.</p>
+                      <p>Worked for a IoT company in the development of applications for multiple types of Android
+                        devices, including Smartphones, Tablets, TVs, and custom devices.</p>
 
-                      <p>Some apps were developed using multi-platform frameworks to speed up development, such as Ionic and Kotlin Multiplatform. We also explored real-time data streaming technologies.</p>
+                      <p>Some apps were developed using multi-platform frameworks to speed up development, such as Ionic
+                        and Kotlin Multiplatform. We also explored real-time data streaming technologies.</p>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="cbp_tmicon education">
+                    <FontAwesomeIcon icon={faGraduationCap} color="white" />
+                  </div>
+                  <div className="cbp_tmlabel wow fadeInRight animated education"
+                       style={{
+                         backgroundImage: 'url(' + process.env.PUBLIC_URL + '/dark_gray_gradient.svg)',
+                         paddingTop: '12px',
+                         paddingBottom: '24px',
+                         minHeight: '0',
+                       }}>
+                    <div className="projectParagraph">
+                      <p>In 2020, I got my <strong style={{fontWeight: 'bold'}}>Bachelor&apos;s in Computer Science</strong>, from the University of Algarve, in Faro,
+                        Portugal.</p>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="cbp_tmicon other">
+                    <FontAwesomeIcon icon={faEllipsisH} color="white" />
+                  </div>
+                  <div className="cbp_tmlabel wow fadeInRight animated"
+                       style={{
+                         backgroundImage: 'url(' + process.env.PUBLIC_URL + '/dark_gray_gradient.svg)',
+                         paddingTop: '12px',
+                         paddingBottom: '24px',
+                         minHeight: '0',
+                       }}>
+                    <div className="projectParagraph">
+                      <p>I discovered my passion for programming when I was around 12 years old. I started by
+                        programming simple flash games, and plugins for video games.</p>
+                      <p>Since then, I have worked on some personal projects, contributed to some open-source projects
+                        (such as the <a href="https://github.com/GitLiveApp/firebase-kotlin-sdk" target="_blank"
+                                    rel="noreferrer">Firebase Kotlin SDK</a>),
+                        and participated in a few Hackathons (such as <a
+                          href="https://tecnico.ulisboa.pt/pt/eventos/fenixhack/" target="_blank"
+                          rel="noreferrer">Fénixhack 2020</a>), Conferences (such as <a
+                          href="https://blog.jetbrains.com/pt-br/kotlin/2023/04/kotlinconf-2023-participe-da-transmissao-ao-vivo-e-tenha-respostas-as-suas-duvidas/"
+                          target="_blank" rel="noreferrer">Kotlin I/O 2023</a>),
+                        and Coding Challenges (such as <a
+                          href="https://blog.google/technology/developers/googles-hash-code-2020/" target="_blank"
+                          rel="noreferrer">Google Hashcode 2020</a>).
+                      </p>
                     </div>
                   </div>
                 </li>
